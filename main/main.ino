9c57c9bd0de6ef4195a9a6a68842db2f50c823e9
sage-acteson <sage.acteson@gmail.com>
2020-11-15 17:46:17 -0700

    rotary encoder sketch complete

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,52 @@
-const int buzzerPin = 3;
+//const int buzzerPin = 3;
+int pinA = 3; // Connected to CLK on KY-040
+int pinB = 4; // Connected to DT on KY-040
+int encoderPosCount = 0;
+int pinALast;
+int aVal;
+boolean bCW;
 
 void setup() {
-  pinMode(buzzerPin, OUTPUT);
+  //  pinMode(buzzerPin, OUTPUT);
+  pinMode (pinA, INPUT);
+  pinMode (pinB, INPUT);
+  /* Read Pin AWhatever state it's in will reflect the last position   */
+  pinALast = digitalRead(pinA);
+  Serial.begin (9600);
 }
 
-void loop() 
+void loop()
 {
-  
+  aVal = digitalRead(pinA);
+  if (aVal != pinALast) { // Means the knob is rotating
+    // if the knob is rotating, we need to determine direction
+    // We do that by reading pin B.
+    if (digitalRead(pinB) != aVal)
+    {
+      // Means pin A Changed first -We're RotatingClockwise.
+      encoderPosCount ++;
+      bCW = true;
+    }
+    else
+    { // Otherwise B changedfirst and we're moving CCW
+      bCW = false;
+      encoderPosCount--;
+    }
+    Serial.print ("Rotated: ");
+    if (bCW) {
+      Serial.println ("clockwise");
+    } else {
+      Serial.println("counterclockwise");
+    } Serial.print("Encoder Position: "); Serial.println(encoderPosCount);
+  } pinALast = aVal;
 }
 
 void audioWarning() {
-  tone(buzzerPin,294,125);//D4
-  delay(125);
-  tone(buzzerPin,294,125);//D4
-  delay(125);
-  tone(buzzerPin,587,250);//D5
-  delay(250);
-  tone(buzzerPin,440,1000);//A4
+//  tone(buzzerPin, 294, 125); //D4
+//  delay(125);
+//  tone(buzzerPin, 294, 125); //D4
+//  delay(125);
+//  tone(buzzerPin, 587, 250); //D5
+//  delay(250);
+//  tone(buzzerPin, 440, 1000); //A4
 }
