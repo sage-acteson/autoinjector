62da013db7ee380c48f3bd2a85be9257fc0ca62b
sage-acteson <sage.acteson@gmail.com>
2020-11-15 17:30:10 -0700

    buzzer code complete

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+const int buzzerPin = 3;
 
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  
+}
 
+void audioWarning() {
+  tone(buzzerPin,294,125);//D4
+  delay(125);
+  tone(buzzerPin,294,125);//D4
+  delay(125);
+  tone(buzzerPin,587,250);//D5
+  delay(250);
+  tone(buzzerPin,440,1000);//A4
 }
