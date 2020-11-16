2d1ad60d0af2cf249dfb183843f1f0f2f005d6d5
sage-acteson <sage.acteson@gmail.com>
2020-11-15 21:38:52 -0700

    potentiometer can set off the piezo

--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,32 @@
 const int buzzerPin = 3;
 const byte heartBeatPin = A4;
+const int heartInterruptPin = 2; // looks like this will work on the nano and feather?
+volatile bool state = false;
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
+  pinMode(heartInterruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(heartInterruptPin), audioBlip, CHANGE);
   Serial.begin (9600);
 }
 
 void loop()
 {
-  int sensorValue = analogRead(A4); // 0 to 708
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(sensorValue);
+//    POTENTIOMETER TEST
+//    int sensorValue = analogRead(heartBeatPin); // 0 to 708
+//    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+//    float voltage = sensorValue * (5.0 / 1023.0);
+//    // print out the value you read:
+//    Serial.println(voltage);
+
+  if (state == true) {
+    state = false;
+    audioWarning();
+  }
+
+//  BUZZER TEST
+//  audioWarning();
+//  delay(1000);
 }
 
 void audioWarning() {
@@ -27,4 +41,8 @@ void audioWarning() {
 
 void audioBlip() {
   tone(buzzerPin, 2000, 10);
+}
+
+void flip() {
+  state = !state;
 }
