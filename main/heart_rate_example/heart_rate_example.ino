7d0ba22c408952aad96cb32196670dbe7b1ba269
sage-acteson <sage.acteson@gmail.com>
2020-11-16 22:26:50 -0700

    short and long avg sent over serial

--- main/heart_rate_example/heart_rate_example.ino
+++ main/heart_rate_example/heart_rate_example.ino
@@ -3,6 +3,14 @@ const int heartInterruptPin = 2;
 
 volatile bool heartRateState = false;
 
+// heart rate tracking
+const int beatsToTrack = 16;
+const int beatsForShortAvg = 6;
+int heartBeats[beatsToTrack]; // store the heart beats as time since the previous beat
+unsigned long timeOfLastBeat = 0;
+float longAvg = 0; // the long and short term average heart rates
+float shortAvg = 0;
+
 void setup() {
   pinMode(buzzerPin, OUTPUT);
   pinMode(heartInterruptPin, INPUT_PULLUP);
@@ -15,6 +23,9 @@ void setup() {
 void loop() {
   if (heartRateState == true) {
     notifyHeartBeat();
+    updateHeartData();
+    calcNewAvgs();
+    Serial.println("LONG: " + String(longAvg) + " SHORT: " + String(shortAvg));
     delay(150);
     heartRateState = false;
   }
@@ -23,6 +34,41 @@ void loop() {
 void notifyHeartBeat() {
   Serial.println("HEARTBEAT DETECTED");
   tone(buzzerPin, 2000, 10);
+}
+
+void updateHeartData() {
+  unsigned long currentTime = millis();
+  int newBeatDuration = currentTime - timeOfLastBeat;
+  // this check is to prevent wild averages if the system is running and isn't connected for a while
+  if (newBeatDuration < 10000) {
+    rotateArray();
+    heartBeats[beatsToTrack - 1] = newBeatDuration;
+  }
+  // TODO (if testing supports it) wipe the array if it has been too long since a heartbeat?
+  timeOfLastBeat = currentTime;
+}
+
+void rotateArray() {
+  int i = 0;
+  for ( i = 0; i < beatsToTrack - 1; i++ ) {
+    heartBeats[i] = heartBeats[i + 1];
+  }
+}
+
+void calcNewAvgs() {
+  longAvg = calcArrayAvg(0, beatsToTrack - beatsForShortAvg - 1);
+  shortAvg = calcArrayAvg(beatsToTrack - beatsForShortAvg, beatsToTrack - 1);
+}
+
+float calcArrayAvg(int startIndex, int endIndex) {
+  int i = 0;
+  int totalTime = 0;
+  int numIndexes = 0;
+  for (i = startIndex; i <= endIndex; i++) {
+    totalTime += heartBeats[i];
+    numIndexes++;
+  }
+  return (totalTime / numIndexes);
 }
 
 void flipHeartRateState() {
