190924e53fbb5fe5e8d26a11c13ad5ac1a8ecc02
sage-acteson <sage.acteson@gmail.com>
2020-11-16 21:44:17 -0700

    button can cycle state - no interrupt

--- main/button_example/button_example.ino
+++ main/button_example/button_example.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int buttonInPin = 3;
+const int buzzerPin = 5;
+
+int currentButtonState = 0;
+int lastButtonState = 0;
+bool currentlyWarning = false;
+
+void setup() {
+  pinMode(buttonInPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  currentButtonState = digitalRead(buttonInPin);
+  // execute logice if the button state has changed
+  if(currentButtonState != lastButtonState) {
+    // but only if it was pressed (no need to do it for unpressing too)
+    if (currentButtonState == HIGH) {
+      handleButtonPress();
+    }
+    delay(30);
+  }
+  lastButtonState = currentButtonState;
 
+  
+}
+
+void handleButtonPress() {
+  if(currentlyWarning == false) {
+    warnUser();
+  } else {
+    cancelInjection();
+  }
+}
+
+void warnUser() {
+  currentlyWarning = true;
+  tone(buzzerPin, 1000, 2000);
+}
+
+void cancelInjection() {
+  currentlyWarning = false;
+  noTone(buzzerPin);
+}
+
+void inject() {
+  
 }
